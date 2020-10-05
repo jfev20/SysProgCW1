3f42c52ffe8862cefdee72f570861290e45250d2
jfev20 <jfev20@googlemail.com>
2020-10-05 12:03:46 +0100

    fixed errors found in basic testing

--- date.c
+++ date.c
@@ -1,31 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <date.h>
+#include "date.h"
 
 typedef struct date {
     int dd, mm, yy;
 } Date;
 
+int valid_date(int d, int m, int y); // declaring date validation method 
+int leap_year(int year); // declaring leap year validation method 
+
 int main() {    // test function - remove when submitting
 
-    char datestr[11] = "28/02/2019";
+    char datestr1[11] = "28/02/2019";
+    char datestr2[11] = "16/07/2018";
 
     printf("-------- TEST DATE CREATION --------\n");
-    Date *outdate = date_create(datestr);
-    if (outdate == NULL) {
-        prinf("Date could not be created\n");
-    } else {
-        printf("Date created: %d/%d/%d\n", outdate->dd, outdate->mm, outdate->yy);
-    }
+    Date *outdate1 = date_create(datestr1);
+    //if (outdate1 == NULL) {
+    //    printf("Date 1 could not be created\n");
+    //} else {
+    //    printf("Date 1 created: %d/%d/%d\n", outdate1->dd, outdate1->mm, outdate1->yy);
+    //}
+    outdate1 == NULL ? printf("Date 1 could not be created\n") : printf("Date 1 created: %d/%d/%d\n", outdate1->dd, outdate1->mm, outdate1->yy);
+
+    Date *outdate2 = date_create(datestr2);
+    outdate2 == NULL ? printf("Date 2 could not be created\n") : printf("Date 2 created: %d/%d/%d\n", outdate2->dd, outdate2->mm, outdate2->yy);
+
+    printf("-------- TEST DATE DUPLICATION --------\n");
+    Date *outdate3 = date_duplicate(outdate1);
+    outdate3 == NULL ? printf("Date 1 could not be duplicated\n") : printf("Date 3 created: %d/%d/%d\n", outdate3->dd, outdate3->mm, outdate3->yy);
+
+    printf("------- TEST DATE COMPARISON ---------\n");
+    int compResult = date_compare(outdate1, outdate2);
+    printf("%d/%d/%d is sooner than %d/%d/%d, the result should be 1: %d\n", outdate1->dd, outdate1->mm, outdate1->yy, outdate2->dd, outdate2->mm, outdate2->yy, compResult);
+    compResult = date_compare(outdate1, outdate3);
+    printf("%d/%d/%d is the same as %d/%d/%d, the result should be 0: %d\n", outdate1->dd, outdate1->mm, outdate1->yy, outdate3->dd, outdate3->mm, outdate3->yy, compResult);
+    compResult = date_compare(outdate2, outdate1);
+    printf("%d/%d/%d is older than %d/%d/%d, the result should be -1: %d\n", outdate2->dd, outdate2->mm, outdate2->yy, outdate1->dd, outdate1->mm, outdate1->yy, compResult);
 
     printf("-------- TEST DATE DESTRUCTION --------\n");
-    date_destory(outdate);
-    printf("end of program");
+    date_destroy(outdate1);
+    printf("end of program\n");
 }
 
 Date *date_create(char *datestr){
     /**
      * QUESTIONS
-     * should we only accept / as the separator?
      * Should the position of the separators always be the same (pos 2 & 5)?
      * Is there a limit of the years we should accept? (e.g. 1990 to 2020)
      * does date_duplication have any validation?
@@ -39,11 +60,11 @@ Date *date_create(char *datestr){
     sep1 = strchr(datestr, '/');    // search for first occurance of / starting from beginning of string
 
     if (sep1 == NULL) {             // if / not found
-        printf("Invalid date format: first / missing");
+        printf("Invalid date format: first / missing\n");
         return NULL;                // return null and quit
     }
 
-    index = (int)(sep2 - datestr);  // store position of first /
+    index = (int)(sep1 - datestr);  // store position of first /
     printf("Position of first / is %d\n", index);
 
     sep2 = strchr(sep1 + 1, '/');   // search for second occurance of / starting from char after first /
@@ -54,7 +75,7 @@ Date *date_create(char *datestr){
     } 
 
     index = (int)(sep2 - datestr);  // store position of second /
-    printf("Position of second / is %d", index);
+    printf("Position of second / is %d\n", index);
 
     int day, month, year = 0;       // initialise days, months and years
     char* endptr;
@@ -65,7 +86,7 @@ Date *date_create(char *datestr){
         printf("Invalid characters before first /\n");
         return NULL;
     }
-    printf("Day: %d", day);
+    printf("Day: %d\n", day);
 
     month = strtol(sep1 + 1, &endptr, 10); // convert after first / (months) to int
 
@@ -73,7 +94,7 @@ Date *date_create(char *datestr){
         printf("Invalid characters before second /\n");
         return NULL;
     }
-    printf("Month: %d", month);
+    printf("Month: %d\n", month);
 
     year = strtol(sep2 + 1, &endptr, 10); // convert last part of string (years) to int
 
@@ -81,16 +102,16 @@ Date *date_create(char *datestr){
         printf("Invalid characters at end of date\n");
         return NULL;
     }
-    printf("Year: %d", year);
+    printf("Year: %d\n", year);
     
     int result = valid_date(day, month, year);  // check if final result is a valid date (e.g. 29th Feb only on leap years/no 33rd of months/max 12 months etc)
 
     if (result == 0) {              // if date validation fails
-        printf("Invalid date");     
+        printf("Invalid date\n");     
         return NULL;                // return null and quit
     }
 
-    printf("Date valid");
+    printf("Date valid\n");
 
 	Date* gooddate = malloc(sizeof(Date));      // allocate memory for date structure
 
@@ -114,10 +135,9 @@ Date *date_duplicate(Date *d){
         return NULL;                            // return null and quit
     }
 
-    int dupday, dupmonth, dupyear = 0;          // initialise new values for duplicate date
-    d->dd = dupday;
-    d->mm = dupmonth;
-    d->yy = dupyear;
+    int dupday = d->dd;          // initialise new values for duplicate date
+    int dupmonth = d->mm;
+    int dupyear = d->yy;
 
     dupdate->dd = dupday;                       // set new values to duplicate date structure
     dupdate->mm = dupmonth;
@@ -130,46 +150,43 @@ Date *date_duplicate(Date *d){
  * date_compare compares two dates, returning <0, 0, >0 if
  * date1<date2, date1==date2, date1>date2, respectively
  */
-int date_compare(Date *date1, Date *date2){
-    int day1, month1, year1 = 0;                // initialise new variables for date1 values
-    date1->dd = day1;
-    date1->mm = month1;
-    date1->yy = year1;
+int date_compare(Date *date1, Date *date2){ // finish this: only returns 0
+    int day1 = date1->dd;
+    int month1 = date1->mm;
+    int year1 = date1->yy;                // initialise new variables for date1 values
 
-    int day2, month2, year2 = 0;                // initialise new variables for date2 values
-    date2->dd = day2;
-    date2->mm = month2;
-    date2->yy = year2;
+    int day2 = date2->dd;
+    int month2 = date2->mm;
+    int year2 = date2->yy;                // initialise new variables for date2 values
+
+    int result = -2;
 
     if (year1 < year2) {            // if the first year is older than the second year then return -1
-        return -1;
+        result = -1;
     } else if (year1 > year2) {     // if the first year is more recent than the second year return 1
-        return 1;
+        result = 1;
     } else if (year1 == year2) {    // if the years are the same...
         if (month1 < month2) {      // ...check if the first month is older than the second month then return -1
-            return -1;
+            result = -1;
         } else if (month1 > month2) { // if the first month is more recent that the second month return 1
-            return 1;
+            result = 1;
         } else if (month1 == month2) {  // if the months are the same...
             if (day1 < day2) {          // ...check if the first day is older that the second day, return -1
-                return -1;
+                result = -1;
             } else if (day1 > day2) {   // if the first day is more recent that the second day, return 1
-                return 1;
+                result = 1;
             } else {
-                return 0;               // the dates are exactly the same, return 0
+                result = 0;               // the dates are exactly the same, return 0
             }
         }
     }
-
-    prinf("Error: comparison failed");  // if the above checks fail, return null
-    return NULL;
-
-
+    return result;
 }
 
 void date_destroy(Date *d){
 	printf("About to free storage\n");
 	free(d);                                    // free up memory at pointer location
+    d = NULL;
 	printf("Storage freed\n");
 }
 
